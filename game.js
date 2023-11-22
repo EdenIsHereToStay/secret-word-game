85b980b0eb12fb0f523ca9fb789777b3a903d293
Edster <125619633+EdenIsHereToStay@users.noreply.github.com>
2023-11-22 00:31:57 -0500

    Update game.js
    Based on your code, the issue might be occurring due to the event listener for the "Submit Guess" button being set up inside the click event listener of the "Start Game" button. This could be causing multiple event listeners to be attached to the "Submit Guess" button, leading to unintended behavior.
    
    To fix this, we should ensure that the event listener for the "Submit Guess" button is set up independently and only once. Let's restructure your JavaScript code accordingly.

--- game.js
+++ game.js
@@ -1,24 +1,27 @@
+let attempts = 5;
+const secretWord = 'tomato';
+
 document.getElementById("startButton").addEventListener("click", function() {
     document.getElementById("gameArea").style.display = "block"; // Show game area
-    let attempts = 5;
+    attempts = 5; // Reset attempts each time game starts
+    document.getElementById("userGuess").value = ''; // Clear previous guess
     document.getElementById("attemptsInfo").textContent = `You have ${attempts} tries.`;
+    document.getElementById("resultMessage").textContent = ''; // Clear previous message
+});
 
-    document.getElementById("submitGuess").addEventListener("click", function() {
-        const secretWord = 'tomato';
-        let userGuess = document.getElementById("userGuess").value;
+document.getElementById("submitGuess").addEventListener("click", function() {
+    let userGuess = document.getElementById("userGuess").value;
 
-        if (userGuess.toLowerCase() === secretWord) {
-            document.getElementById("resultMessage").textContent = 'Welcome to the secret loop world!';
-            document.getElementById("gameArea").style.display = "none"; // Hide game area after correct guess
-            return; // Exit the function to prevent further code execution
-        }
-
+    if (userGuess.toLowerCase() === secretWord) {
+        document.getElementById("resultMessage").textContent = 'Welcome to the secret loop world!';
+        document.getElementById("gameArea").style.display = "none";
+    } else {
         attempts--;
         if (attempts > 0) {
             document.getElementById("attemptsInfo").textContent = `You have ${attempts} tries left.`;
         } else {
             document.getElementById("resultMessage").textContent = 'Access denied :(';
-            document.getElementById("gameArea").style.display = "none"; // Hide game area after attempts are over
+            document.getElementById("gameArea").style.display = "none";
         }
-    });
+    }
 });
