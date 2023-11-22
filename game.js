a517644bef98fe5f28b431968f071953f3a2e432
Edster <125619633+EdenIsHereToStay@users.noreply.github.com>
2023-11-22 00:28:49 -0500

    Update game.js
    fixed issues in the code that prevented correct funcitonality.

--- game.js
+++ game.js
@@ -1,6 +1,5 @@
-// game.js
 document.getElementById("startButton").addEventListener("click", function() {
-    document.getElementById("gameArea").style.display = "block";
+    document.getElementById("gameArea").style.display = "block"; // Show game area
     let attempts = 5;
     document.getElementById("attemptsInfo").textContent = `You have ${attempts} tries.`;
 
@@ -10,14 +9,16 @@ document.getElementById("startButton").addEventListener("click", function() {
 
         if (userGuess.toLowerCase() === secretWord) {
             document.getElementById("resultMessage").textContent = 'Welcome to the secret loop world!';
-            document.getElementById("gameArea").style.display = "none";
-        } else {
-            attempts--;
+            document.getElementById("gameArea").style.display = "none"; // Hide game area after correct guess
+            return; // Exit the function to prevent further code execution
+        }
+
+        attempts--;
+        if (attempts > 0) {
             document.getElementById("attemptsInfo").textContent = `You have ${attempts} tries left.`;
-            if (attempts <= 0) {
-                document.getElementById("resultMessage").textContent = 'Access denied :(';
-                document.getElementById("gameArea").style.display = "none";
-            }
+        } else {
+            document.getElementById("resultMessage").textContent = 'Access denied :(';
+            document.getElementById("gameArea").style.display = "none"; // Hide game area after attempts are over
         }
     });
 });
