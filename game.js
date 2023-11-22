e5f82c13ce4d23cf9e7b3136a5c90bb44c917a9f
Edster <125619633+EdenIsHereToStay@users.noreply.github.com>
2023-11-22 00:38:05 -0500

    Update game.js
    Certainly! Let's redesign the Secret Word Guessing Game with a focus on best practices, clear structure, and well-commented code. I'll provide a more interactive and user-friendly version with an improved user interface.
    
    JavaScript Logic (game.js)
    In game.js, we'll implement the game logic with clear comments explaining each part.

--- game.js
+++ game.js
@@ -1,4 +1,6 @@
+// Wait for the DOM to be fully loaded
 document.addEventListener('DOMContentLoaded', function() {
+    // Initialize game variables
     const secretWord = 'tomato';
     let attempts;
     const gameArea = document.getElementById('gameArea');
@@ -6,23 +8,30 @@ document.addEventListener('DOMContentLoaded', function() {
     const resultMessage = document.getElementById('resultMessage');
     const userGuess = document.getElementById('userGuess');
 
+    // Start the game
     document.getElementById('startButton').addEventListener('click', function() {
+        // Reset the game state
         attempts = 5;
         gameArea.style.display = 'block';
         attemptsInfo.textContent = `You have ${attempts} tries.`;
         resultMessage.textContent = '';
         userGuess.value = '';
+        userGuess.focus(); // Focus on input field
     });
 
+    // Handle guess submission
     document.getElementById('submitGuess').addEventListener('click', function() {
+        // Check if the guess is correct
         if (userGuess.value.toLowerCase() === secretWord) {
             resultMessage.textContent = 'Welcome to the secret loop world!';
             gameArea.style.display = 'none';
         } else {
+            // Decrement attempts and update UI
             attempts--;
             if (attempts > 0) {
                 attemptsInfo.textContent = `You have ${attempts} tries left.`;
             } else {
+                // Handle game over state
                 resultMessage.textContent = 'Access denied :(';
                 gameArea.style.display = 'none';
             }
